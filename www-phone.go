d0fd1f07b501a75c1c8eefe2b13abbf9e7f09203
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2021-03-23 10:13:04 +0200

    Defining the structure

--- www-phone.go
+++ www-phone.go
@@ -1,5 +1,70 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	Body := "Thanks for visiting!\n"
+	fmt.Fprintf(w, "%s", Body)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	Body := "Delete Handler!\n"
+	fmt.Fprintf(w, "%s", Body)
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	Body := "Delete Handler!\n"
+	fmt.Fprintf(w, "%s", Body)
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	Body := "Status Handler!\n"
+	fmt.Fprintf(w, "%s", Body)
+}
+
+func insertHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	Body := "Insert Handler!\n"
+	fmt.Fprintf(w, "%s", Body)
+}
+
 func main() {
+
+	mux := http.NewServeMux()
+	s := &http.Server{
+		Addr:         PORT,
+		Handler:      mux,
+		IdleTimeout:  10 * time.Second,
+		ReadTimeout:  time.Second,
+		WriteTimeout: time.Second,
+	}
+
+	mux.Handle("/list", http.HandlerFunc(listHandler))
+	mux.Handle("/insert", http.HandlerFunc(insertHandler))
+	mux.Handle("/search", http.HandlerFunc(searchHandler))
+	mux.Handle("/delete", http.HandlerFunc(deleteHandler))
+	mux.Handle("/status", http.HandlerFunc(statusHandler))
+	mux.Handle("/", http.HandlerFunc(defaultHandler))
+
+	fmt.Println("Ready to serve at", PORT)
+	err := s.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 }
