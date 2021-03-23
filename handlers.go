faa481a156964e579f446655d8402b92ed018886
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2021-03-23 10:37:01 +0200

    Split main package

---
+++ handlers.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const PORT = "1234"
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	Body := "Thanks for visiting!\n"
+	fmt.Fprintf(w, "%s", Body)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	Body := "Delete Handler!\n"
+	fmt.Fprintf(w, "%s", Body)
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	Body := "Delete Handler!\n"
+	fmt.Fprintf(w, "%s", Body)
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	Body := "Status Handler!\n"
+	fmt.Fprintf(w, "%s", Body)
+}
+
+func insertHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	Body := "Insert Handler!\n"
+	fmt.Fprintf(w, "%s", Body)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	Body := "Search Handler!\n"
+	fmt.Fprintf(w, "%s", Body)
+}
--- www-phone.go
+++ www-phone.go
@@ -1,48 +1,159 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	"log"
 	"net/http"
+	"os"
+	"regexp"
+	"strconv"
 	"time"
 )
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving:", r.URL.Path, "from", r.Host)
-	w.WriteHeader(http.StatusOK)
-	Body := "Thanks for visiting!\n"
-	fmt.Fprintf(w, "%s", Body)
+type Entry struct {
+	Name       string
+	Surname    string
+	Tel        string
+	LastAccess string
 }
 
-func deleteHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving:", r.URL.Path, "from", r.Host)
-	w.WriteHeader(http.StatusOK)
-	Body := "Delete Handler!\n"
-	fmt.Fprintf(w, "%s", Body)
+// JSONFILE resides in the current directory
+var CSVFILE = "./data.csv"
+
+type PhoneBook []Entry
+
+var data = PhoneBook{}
+var index map[string]int
+
+func readCSVFile(filepath string) error {
+	_, err := os.Stat(filepath)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// CSV file read all at once
+	lines, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		return err
+	}
+
+	for _, line := range lines {
+		temp := Entry{
+			Name:       line[0],
+			Surname:    line[1],
+			Tel:        line[2],
+			LastAccess: line[3],
+		}
+		// Storing to global variable
+		data = append(data, temp)
+	}
+
+	return nil
 }
 
-func listHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving:", r.URL.Path, "from", r.Host)
-	w.WriteHeader(http.StatusOK)
-	Body := "Delete Handler!\n"
-	fmt.Fprintf(w, "%s", Body)
+func saveCSVFile(filepath string) error {
+	csvfile, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer csvfile.Close()
+
+	csvwriter := csv.NewWriter(csvfile)
+	for _, row := range data {
+		temp := []string{row.Name, row.Surname, row.Tel, row.LastAccess}
+		_ = csvwriter.Write(temp)
+	}
+	csvwriter.Flush()
+	return nil
 }
 
-func statusHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving:", r.URL.Path, "from", r.Host)
-	w.WriteHeader(http.StatusOK)
-	Body := "Status Handler!\n"
-	fmt.Fprintf(w, "%s", Body)
+func createIndex() error {
+	index = make(map[string]int)
+	for i, k := range data {
+		key := k.Tel
+		index[key] = i
+	}
+	return nil
 }
 
-func insertHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving:", r.URL.Path, "from", r.Host)
-	w.WriteHeader(http.StatusOK)
-	Body := "Insert Handler!\n"
-	fmt.Fprintf(w, "%s", Body)
+// Initialized by the user – returns a pointer
+// If it returns nil, there was an error
+func initS(N, S, T string) *Entry {
+	// Both of them should have a value
+	if T == "" || S == "" {
+		return nil
+	}
+	// Give LastAccess a value
+	LastAccess := strconv.FormatInt(time.Now().Unix(), 10)
+	return &Entry{Name: N, Surname: S, Tel: T, LastAccess: LastAccess}
+}
+
+func insert(pS *Entry) error {
+	// If it already exists, do not add it
+	_, ok := index[(*pS).Tel]
+	if ok {
+		return fmt.Errorf("%s already exists", pS.Tel)
+	}
+	data = append(data, *pS)
+	// Update the index
+	_ = createIndex()
+
+	err := saveCSVFile(CSVFILE)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func deleteEntry(key string) error {
+	i, ok := index[key]
+	if !ok {
+		return fmt.Errorf("%s cannot be found!", key)
+	}
+	data = append(data[:i], data[i+1:]...)
+	// Update the index - key does not exist any more
+	delete(index, key)
+
+	err := saveCSVFile(CSVFILE)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func search(key string) *Entry {
+	i, ok := index[key]
+	if !ok {
+		return nil
+	}
+	data[i].LastAccess = strconv.FormatInt(time.Now().Unix(), 10)
+	return &data[i]
+}
+
+func matchTel(s string) bool {
+	t := []byte(s)
+	re := regexp.MustCompile(`\d+$`)
+	return re.Match(t)
 }
 
 func main() {
+	err := readCSVFile(CSVFILE)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = createIndex()
+	if err != nil {
+		fmt.Println("Cannot create index.")
+		return
+	}
 
 	mux := http.NewServeMux()
 	s := &http.Server{
@@ -61,10 +172,9 @@ func main() {
 	mux.Handle("/", http.HandlerFunc(defaultHandler))
 
 	fmt.Println("Ready to serve at", PORT)
-	err := s.ListenAndServe()
+	err = s.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
 }
