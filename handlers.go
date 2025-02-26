d8238b1ec27593b641a6a6cfb4ec7bb8689c3341
oni <oniengineer521@gmail.com>
2025-02-26 05:05:35 -0700

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -7,116 +7,117 @@ import (
 	"strings"
 )
 
-const PORT = ":1234"
+const PORT = ":8080"
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Serving:", r.URL.Path, "from", r.Host)
 	w.WriteHeader(http.StatusOK)
-	Body := "Thanks for visiting!\n"
-	fmt.Fprintf(w, "%s", Body)
+	body := "thanks for visiting!\n"
+	fmt.Fprintf(w, "%s", body)
 }
 
 func deleteHandler(w http.ResponseWriter, r *http.Request) {
-	// Get telephone
 	paramStr := strings.Split(r.URL.Path, "/")
 	fmt.Println("Path:", paramStr)
+
 	if len(paramStr) < 3 {
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintln(w, "Not found: "+r.URL.Path)
+		fmt.Fprintf(w, "Not found: %s", r.URL.Path)
 		return
 	}
 
 	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	phone := paramStr[2]
+	err := deleteEntry(phone)
 
-	telephone := paramStr[2]
-	err := deleteEntry(telephone)
 	if err != nil {
 		fmt.Println(err)
-		Body := err.Error() + "\n"
+		body := err.Error() + "\n"
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "%s", Body)
+		fmt.Fprintf(w, "%s", body)
 		return
 	}
 
-	Body := telephone + " deleted!\n"
+	body := phone + " deleted!\n"
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "%s", Body)
+	fmt.Fprintf(w, "%s", body)
 }
 
 func listHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Serving:", r.URL.Path, "from", r.Host)
 	w.WriteHeader(http.StatusOK)
-	Body := list()
-	fmt.Fprintf(w, "%s", Body)
+	body := list()
+	fmt.Fprintf(w, "%s", body)
 }
 
 func statusHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Serving:", r.URL.Path, "from", r.Host)
 	w.WriteHeader(http.StatusOK)
-	Body := fmt.Sprintf("Total entries: %d\n", len(data))
-	fmt.Fprintf(w, "%s", Body)
+	body := fmt.Sprintf("Total entries: %d\n", len(data))
+	fmt.Fprintf(w, "%s", body)
 }
 
 func insertHandler(w http.ResponseWriter, r *http.Request) {
-	// Split URL
 	paramStr := strings.Split(r.URL.Path, "/")
 	fmt.Println("Path:", paramStr)
 
 	if len(paramStr) < 5 {
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintln(w, "Not enough arguments: "+r.URL.Path)
+		fmt.Fprintf(w, "Not enough arguments: %s", r.URL.Path)
 		return
 	}
 
 	name := paramStr[2]
 	surname := paramStr[3]
-	tel := paramStr[4]
+	phone := paramStr[4]
 
-	t := strings.ReplaceAll(tel, "-", "")
-	if !matchTel(t) {
-		fmt.Println("Not a valid telephone number:", tel)
+	p := strings.ReplaceAll(phone, "-", "")
+
+	if !matchTel(p) {
+		fmt.Println("Not a valid phone number:", p, " | ", phone)
 		return
 	}
 
-	temp := &Entry{Name: name, Surname: surname, Tel: t}
+	temp := &Entry{Name: name, Surname: surname, Tel: p}
 	err := insert(temp)
 
 	if err != nil {
 		w.WriteHeader(http.StatusNotModified)
-		Body := "Failed to add record\n"
-		fmt.Fprintf(w, "%s", Body)
+		body := "failed to add record\n"
+		fmt.Fprintf(w, "%s", body)
 	} else {
 		log.Println("Serving:", r.URL.Path, "from", r.Host)
-		Body := "New record added successfully\n"
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "%s", Body)
+		body := "new record added successfully\n"
+		fmt.Fprintf(w, "%s", body)
 	}
 
 	log.Println("Serving:", r.URL.Path, "from", r.Host)
 }
 
 func searchHandler(w http.ResponseWriter, r *http.Request) {
-	// Get Search value from URL
 	paramStr := strings.Split(r.URL.Path, "/")
 	fmt.Println("Path:", paramStr)
 
 	if len(paramStr) < 3 {
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintln(w, "Not found: "+r.URL.Path)
+		fmt.Fprintf(w, "Not found: %s", r.URL.Path)
 		return
 	}
 
-	var Body string
-	telephone := paramStr[2]
-	t := search(telephone)
-	if t == nil {
+	var body string
+	phone := paramStr[2]
+
+	p := search(phone)
+
+	if p == nil {
 		w.WriteHeader(http.StatusNotFound)
-		Body = "Could not be found: " + telephone + "\n"
+		body = fmt.Sprintf("could not be found: %s\n", phone)
 	} else {
 		w.WriteHeader(http.StatusOK)
-		Body = t.Name + " " + t.Surname + " " + t.Tel + "\n"
+		body = fmt.Sprintf("%s %s %s \n", p.Name, p.Surname, p.Tel)
 	}
 
 	fmt.Println("Serving:", r.URL.Path, "from", r.Host)
-	fmt.Fprintf(w, "%s", Body)
+	fmt.Fprintf(w, "%s", body)
 }
+
